85eb9827a859a80837eadbae5fd951ee4c5cf1c7
Julia Hormuth <jvhormuth@gmail.com>
2021-11-28 19:32:53 -0300

    added shift and pop methods

---
+++ pop.js
@@ -0,0 +1,12 @@
+// Pop
+
+// allows removing elements from the end of the array
+
+const array = [ 1, 2, 3, 4, 5]
+const remove = array.pop();  // receives the value taken from the array
+
+console.log(remove)
+console.log(array)
+
+
+
