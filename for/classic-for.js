03f22968bf0d677e28234b31c0f812ead83490fb
Julia Hormuth <jvhormuth@gmail.com>
2022-06-12 22:16:21 -0300

    classic for

---
+++ for/classic-for.js
@@ -0,0 +1,16 @@
+// Classic For
+
+// i - index
+for (let i = 0; i <=5; i++) { //executes when the condition is true
+ console.log(i)
+}
+
+for (let i = 500; i >=400; i-=10) {
+    console.log(i)
+}
+
+const fruits = ['litter', 'pear', 'grape']
+
+for (let i = 0; i < fruits.length; i++) {
+console.log(`Index ${i}`, fruits[i])
+}
