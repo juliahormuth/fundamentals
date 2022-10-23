36efcc8288e528add02536efa411550c9f1bd816
Julia Hormuth <jvhormuth@gmail.com>
2022-10-23 20:53:59 -0300

    fix object map

--- map/map.js
+++ map/map.js
@@ -1,14 +1,28 @@
 const pessoas = [
-    { id: 3, nome: 'Julia' },
-    { id: 2, nome: 'Daniel' },
-    { id: 1, nome: 'Felipe' },
-]
+  { id: 3, nome: "Julia" },
+  { id: 2, nome: "Daniel" },
+  { id: 1, nome: "Felipe" },
+];
 
-const novasPessoas = {}
+const novasPessoas = {};
 
 for (const pessoa of pessoas) {
-    const { id } = pessoas
-    novasPessoas[id] = { ...pessoa }
+  const { id } = pessoas;
+  novasPessoas[id] = { ...pessoa };
 }
 
-console.log(novasPessoas)
+const products = new Map();
+
+products.set("teste", "Mouse");
+products.set("2", "Monitor");
+products.set("3", "Teclado");
+
+console.log(products);
+console.log(products.size);
+
+console.log(products.get("teste")); //Mouse
+
+for (p of products) {
+  console.log(p[1]); // The products without keys
+  // p[0] come the keys
+}
